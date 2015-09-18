bb0bc9f40f1e845702ba257ee322f34cff829a11
K Subramanian <ksubramanian@Ks-MacBook-Air.local>
2015-09-19 00:42:49 +0530

    add __init__ with info add setup.py

---
+++ drf_eventlog/__init__.py
@@ -0,0 +1,6 @@
+__title__ = 'DRF Eventlog'
+__version__ = '0.0.1'
+__author__ = 'K Subramanian'
+
+# Version synonym
+VERSION = __version__
---
+++ setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+version = get_version('drf_eventlog')
+
+setup(
+    name='drf_eventlog',
+    version=version,
+    description='Log events in your app and get REST APIs with filtering for them.',
+    author='K Subramanian',
+    author_email='baathai@gmail.com',  
+    packages=get_packages('drf_eventlog'),
+    package_data=get_package_data('drf_eventlog'),
+    install_requires=[],
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
