d910213bb5ba42dc17684154351428f605bcfd3a
K Subramanian <ksubramanian@Ks-MacBook-Air.local>
2015-09-19 21:40:46 +0530

    commit inital migration

---
+++ drf_eventlog/0001_initial.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Log',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_id', models.CharField(max_length=36, null=True)),
+                ('event', models.CharField(max_length=50, db_index=True)),
+                ('details', models.TextField(null=True)),
+                ('timestamp', models.BigIntegerField(db_index=True)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', null=True)),
+            ],
+            options={
+                'ordering': ['-timestamp'],
+            },
+        ),
+    ]
